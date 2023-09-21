7dbc1c7a38beaf6d893bf67bd7ca261dfc4ded5c
akbar003 <khanakbar0233@gmail.com>
2023-09-21 11:39:07 +0500

    nesred if in dart

---
+++ nestedif.dart
@@ -0,0 +1,15 @@
+void main() {
+  String email = "ab@gmail.com";
+  String pass = "p12";
+  if (email == "ab@gmail.om" || pass == "p2") {
+    if (email == "ab@gmail.com" && pass == "p1") {
+      print("you are authurized");
+    } else if (email == "abgmail.com") {
+      print("pass is must");
+    } else {
+      print("sorry email is must");
+    }
+  } else {
+    print("error");
+  }
+}
